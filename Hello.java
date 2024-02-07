5c680dde7f032c23fc36233dcb6d22e1807bd188
0139450 <159297417+0139450@users.noreply.github.com>
2024-02-07 22:05:22 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ class hello
+{
+    public static void main(String args[])
+    {
+        System.out.println("enetr a name: ");
+        Scanner input=new Scanner(System.in);
+        String name=input.nextLine();
+        System.out.println("hello "+name);
+
+
+    }
+}
