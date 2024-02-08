54f10d26cdc11a94999dba33c84c00ef69e6a2f9
0139450 <159297417+0139450@users.noreply.github.com>
2024-02-08 11:34:32 +0530

    Add files via upload

---
+++ evenorodd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enetr the number a:";
+    cin>>a;
+    if(a%2==0)
+    {
+        cout<<"number is even"<<endl;
+
+    }
+    else{
+        cout<<"number is odd"<<endl;
+    }
+
+}
