37e946eaa07ebf73462b91472f2f3a44355bd755
0139450 <159297417+0139450@users.noreply.github.com>
2024-02-08 11:34:13 +0530

    Add files via upload

---
+++ dm.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    float d=56.678;
+    string n;
+    cout<<"hi "<<endl;
+    cout<<"world"<<ends;
+    cout<<setw(10)<<"gayi"<<endl;
+    cout<<setw(10)<<setfill('*')<<"gayi"<<endl;
+    cout<<setprecision(11)<<d<<endl;
+    cout<<"the world"<<flush<<endl;
+    cout.flush();
+    cout<<"enetr name : ";
+    cin>>ws;
+    cin>>n;
+    cout<<"the name is "<<n<<endl;
+
+}
